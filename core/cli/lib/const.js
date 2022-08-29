edec5d06cf65955dd8aa2653c506a8da1d29f93f
chenzehua <136565629@qq.com>
2022-08-29 15:12:01 +0800

    添加：检查是否需要全局更新 ,检查环境变量 检查入参是否是debug模式

--- core/cli/lib/const.js
+++ core/cli/lib/const.js
@@ -1,5 +1,6 @@
 const LOWEST_NODE_VERSION="12.0.0";
-
+const DEFAULT_CLI_HOME=".czh-cli";
 module.exports={
-    LOWEST_NODE_VERSION
+    LOWEST_NODE_VERSION,
+    DEFAULT_CLI_HOME
 }
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,41 +1,95 @@
 "use strict";
+const path=require('path');
 const semver = require("semver");
-const userHome=require('user-home');
+const userHome = require("user-home");
 const pathExists = require("path-exists").sync;
 const colors = require("colors/safe");
 const pkg = require("../package.json");
 const log = require("@czh-cli-dev/log");
 const constant = require("./const");
 
-
-console.log(pathExists);
+let args,config;
 
 function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
-    // checkRoot(); //后面恢复他只在mac上有用目前
+    // checkRoot(); //后面恢复 目前他只在mac上有用
     checkUserHome();
+    checkInputArgs();
+    checkEnv();
   } catch (e) {
     log.error(e.message);
   }
 }
 
-// 检查是否是root账户启动  注：windows 系统有问题 目前所知只有macOs才生效
-function checkRoot(){
-    // console.log(process.geteuid());
-    const rootCheck=require("root-check");
-    // 切换到root账户
-    rootCheck();
-    //切换到用户主目录
-    checkUserHome();
+
+// 检查是否需要全局更新
+function checkGlobalUpdate(){
+  // 1.获取当前版本号合模块名
+
 }
 
 
-function checkUserHome(){
+
+  // 检查环境变量
+function checkEnv(){
+  const dotenv=require('dotenv');
+  let dotenvPath=path.resolve(userHome,".env")
+    if(pathExists(dotenvPath)){
+       dotenv.config({path:dotenvPath});
+    }
+   createDefaultConfig();
+  
+  log.verbose("环境变量",process.env.CLI_HOME_PATH)
+}
+
+// 创建默认配置
+function  createDefaultConfig(){
+   const cliConifg={
+    home:userHome
+   }
+   if(process.env.CLI_HOME){
+    cliConifg['cliHome']=path.join(userHome,process.env.CLI_HOME)
+   }else{
+    cliConifg['cliHome']=path.join(userHome,constant.DEFAULT_CLI_HOME);
+   }
+  //  第二种
+  process.env.CLI_HOME_PATH=cliConifg.cliHome
+  //  第一种
+  //  return cliConifg;
+}
+
+// 检查入参是否是debug模式
+function checkInputArgs() {
+  const minimist = require("minimist");
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+// 检查参数
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = "verbose";
+  } else {
+    process.env.LOG_LEVEL = "info";
+  }
+  log.level = process.env.LOG_LEVEL;
+}
+
+// 检查是否是root账户启动  注：windows 系统有问题 目前所知只有macOs才生效
+function checkRoot() {
+  // console.log(process.geteuid());
+  const rootCheck = require("root-check");
+  // 切换到root账户
+  rootCheck();
+}
+
+//切换到用户主目录 检查用户主目录是否存在
+function checkUserHome() {
   console.log(userHome);
-  if(!userHome||!pathExists(userHome)){
-    throw new Error(colors.red("当前登录用户主目录不存在!"))
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在!"));
   }
 }
 
