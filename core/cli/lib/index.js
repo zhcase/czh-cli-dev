c639d090f3671da76011b423158a2fd2791698ca
zhcase <ze__hua@163.com>
2022-08-30 21:57:42 +0800

    添加:脚手架准备环境搭建完毕

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,39 +1,42 @@
-"use strict";
-const path=require('path');
-const semver = require("semver");
-const userHome = require("user-home");
-const pathExists = require("path-exists").sync;
-const colors = require("colors/safe");
-const pkg = require("../package.json");
-const log = require("@czh-cli-dev/log");
-const constant = require("./const");
+"use strict"
+const path = require('path')
+const semver = require("semver")
+const userHome = require("user-home")
+const pathExists = require("path-exists").sync
+const colors = require("colors/safe")
+const pkg = require("../package.json")
+const log = require("@czh-cli-dev/log")
+const constant = require("./const")
 
-let args,config;
+let args, config
 
 async function core() {
   try {
-    checkPkgVersion();
-    checkNodeVersion();
+    checkPkgVersion()
+    checkNodeVersion()
     // checkRoot(); //后面恢复 目前他只在mac上有用
-    checkUserHome();
-    checkInputArgs();
-    checkEnv();
-    await checkGlobalUpdate();
+    checkUserHome()
+    checkInputArgs()
+    checkEnv()
+    await checkGlobalUpdate()
   } catch (e) {
-    log.error(e.message);
+    log.error(e.message)
   }
 }
 
 
 // 检查是否需要全局更新
-async function checkGlobalUpdate(){
+async function checkGlobalUpdate() {
   // 1.获取当前版本号合模块名
-  const cuurentVersion=pkg.version;
-  const npmName=pkg.name;
+  const currentVersion = pkg.version
+  const npmName = pkg.name
   // 2.调用Npm Api 获取所有模块
-  const {getNpmInfo}=require("@czh-cli-dev/get-npm-info");
- let data=await getNpmInfo(npmName);
- console.log(data.versions);
+  const { getNpmSemverVersion } = require("@czh-cli-dev/get-npm-info")
+  let lastVersion = await getNpmSemverVersion(currentVersion, npmName)
+  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
+    log.warn(colors.yellow(`请手动更新${npmName},当前版本：${currentVersion},最新版本:${lastVersion}
+    更新命令：npm install -g ${npmName}`))
+  }
   // 3.提取所有版本号，比对哪些版本号是大于当前版本号的
   // 4. 获取最新版本号，提示用户更新到该版本
 
@@ -41,83 +44,83 @@ async function checkGlobalUpdate(){
 
 
 
-  // 检查环境变量
-function checkEnv(){
-  const dotenv=require('dotenv');
-  let dotenvPath=path.resolve(userHome,".env")
-    if(pathExists(dotenvPath)){
-       dotenv.config({path:dotenvPath});
-    }
-   createDefaultConfig();
-  
-  log.verbose("环境变量",process.env.CLI_HOME_PATH)
+// 检查环境变量
+function checkEnv() {
+  const dotenv = require('dotenv')
+  let dotenvPath = path.resolve(userHome, ".env")
+  if (pathExists(dotenvPath)) {
+    dotenv.config({ path: dotenvPath })
+  }
+  createDefaultConfig()
+
+  log.verbose("环境变量", process.env.CLI_HOME_PATH)
 }
 
 // 创建默认配置
-function  createDefaultConfig(){
-   const cliConifg={
-    home:userHome
-   }
-   if(process.env.CLI_HOME){
-    cliConifg['cliHome']=path.join(userHome,process.env.CLI_HOME)
-   }else{
-    cliConifg['cliHome']=path.join(userHome,constant.DEFAULT_CLI_HOME);
-   }
+function createDefaultConfig() {
+  const cliConifg = {
+    home: userHome
+  }
+  if (process.env.CLI_HOME) {
+    cliConifg['cliHome'] = path.join(userHome, process.env.CLI_HOME)
+  } else {
+    cliConifg['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME)
+  }
   //  第二种
-  process.env.CLI_HOME_PATH=cliConifg.cliHome
+  process.env.CLI_HOME_PATH = cliConifg.cliHome
   //  第一种
   //  return cliConifg;
 }
 
 // 检查入参是否是debug模式
 function checkInputArgs() {
-  const minimist = require("minimist");
-  args = minimist(process.argv.slice(2));
-  checkArgs();
+  const minimist = require("minimist")
+  args = minimist(process.argv.slice(2))
+  checkArgs()
 }
 
 // 检查参数
 function checkArgs() {
   if (args.debug) {
-    process.env.LOG_LEVEL = "verbose";
+    process.env.LOG_LEVEL = "verbose"
   } else {
-    process.env.LOG_LEVEL = "info";
+    process.env.LOG_LEVEL = "info"
   }
-  log.level = process.env.LOG_LEVEL;
+  log.level = process.env.LOG_LEVEL
 }
 
 // 检查是否是root账户启动  注：windows 系统有问题 目前所知只有macOs才生效
 function checkRoot() {
   // console.log(process.geteuid());
-  const rootCheck = require("root-check");
+  const rootCheck = require("root-check")
   // 切换到root账户
-  rootCheck();
+  rootCheck()
 }
 
 //切换到用户主目录 检查用户主目录是否存在
 function checkUserHome() {
-  console.log(userHome);
+  console.log(userHome)
   if (!userHome || !pathExists(userHome)) {
-    throw new Error(colors.red("当前登录用户主目录不存在!"));
+    throw new Error(colors.red("当前登录用户主目录不存在!"))
   }
 }
 
 // 检查node版本
 function checkNodeVersion() {
   // 第一步获取当前node版本号
-  const currentVersion = process.version;
+  const currentVersion = process.version
   // 第二步 比对最低版本号
-  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  const lowestVersion = constant.LOWEST_NODE_VERSION
   if (!semver.gte(currentVersion, lowestVersion)) {
     throw new Error(
       colors.red(`czh-cli 需要安装 v${lowestVersion}以上版本的Node.js`)
-    );
+    )
   }
 }
 
 // 检查package版本
 function checkPkgVersion() {
-  log.info("cli", pkg.version);
+  log.info("cli", pkg.version)
 }
 
-module.exports = core;
+module.exports = core
--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -1,33 +1,65 @@
-'use strict';
-const axios =require("axios");
-const urlJoin=require('url-join');
-const semver=require('semver');
+'use strict'
+const axios = require("axios")
+const urlJoin = require('url-join')
+const semver = require('semver')
 
 
-
-function getNpmInfo(npmName,registry) {
-    console.log(npmName);
-    if(!npmName){
+// 获取Npm版本信息
+function getNpmInfo(npmName, registry) {
+    console.log(npmName)
+    if (!npmName) {
         return null
     }
-    const registryUrl=registry||getDefaultRegistry();
-    const npmInfoUrl=urlJoin(registryUrl,npmName);
-    return axios.get(npmInfoUrl).then(res=>{
-        if(res.status===200){
-            return res.data;
-        }else{
-            return null;
+    const registryUrl = registry || getDefaultRegistry()
+    const npmInfoUrl = urlJoin(registryUrl, npmName)
+    return axios.get(npmInfoUrl).then(res => {
+        if (res.status === 200) {
+            return res.data
+        } else {
+            return null
         }
-    }).catch(err=>{
+    }).catch(err => {
         return Promise.reject(err)
     })
 }
 
-function getDefaultRegistry(isOriginal=false){
-    return isOriginal?"https://registry.npmjs.org/":"https://registry.npm.taobao.org/"
+
+// 使用是否淘宝镜像
+function getDefaultRegistry(isOriginal = false) {
+    return isOriginal ? "https://registry.npmjs.org/" : "https://registry.npm.taobao.org/"
+}
+
+// 获取多个版本号
+async function getNpmVersions(npmName, registry) {
+    const data = await getNpmInfo(npmName, registry)
+    if (data) {
+        return Object.keys(data.versions)
+    } else {
+        return []
+    }
+}
+
+// 过滤版本号
+async function getSemverVersions(baseVersion, versions) {
+    versions = versions.filter(version =>
+        semver.satisfies(version, `^${baseVersion}`)
+    ).sort((a, b) => {
+        return semver.gt(b, a)
+    })
+    return versions
 }
 
 
+async function getNpmSemverVersion(baseVersion, npmName, registry) {
+    const versions = await getNpmVersions(npmName, registry)
+    const newVersions = await getSemverVersions(baseVersion, versions)
+    if (newVersions && newVersions.length > 0) {
+        return newVersions[0]
+    }
+}
+
 module.exports = {
-    getNpmInfo
+    getNpmInfo,
+    getNpmVersions,
+    getNpmSemverVersion
 }
