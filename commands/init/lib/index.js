552fb92142ce416eb5e9a6acf47ea7624a3670ac
chenzehua <136565629@qq.com>
2022-09-21 19:58:39 +0800

    添加萱蕚创建组件或者项目

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -2,8 +2,14 @@
 const fs = require('fs')
 const fse = require('fs-extra')
 const inquirer = require('inquirer')
+const semver =require("semver")
 const Command = require('@czh-cli-dev/command')
 const log = require('@czh-cli-dev/log')
+
+
+const TYPE_PROJECT='project';
+const TYPE_COMPONENT="component"
+
 class InitCommand extends Command {
   init() {
     this.projectName = this._argv[0] || ''
@@ -11,15 +17,26 @@ class InitCommand extends Command {
     log.verbose('projectName', this.projectName)
     log.verbose('force', this.force)
   }
-  exec() {
+ async exec() {
     //1. 准备阶段
     try {
-      this.prepare()
+     const projectInfo= await this.prepare()
+     if(projectInfo){
+      //2.下载模板
+      this.downLoadTemplate();
+     }
     } catch (error) {
       log.error(error.message)
     }
     //2.安装模板
     // 3. 安装模板
+  }
+  downLoadTemplate(){
+    //1.通过项目模板api获取项目模板信息
+    //1.1 通过egg.js 搭建一套后端系统
+    //1.2 通过npm 存储项目
+    //1.3 讲项目模板信息存储到mongodb数据库中
+    //1.4 通过egg.js 获取mongodb中的数据并且通过APi返回
   }
 
   async prepare() {
@@ -53,18 +70,103 @@ class InitCommand extends Command {
           default: false,
           message: '是否确认清空当前目录下的文件',
         })
+           //2.是否启动强制更新
         if (confirmDelete) {
           //清空当前目录
           fse.emptyDirSync(localPath)
+        }else{
+          return;
         }
       }
     }
-    //2.是否启动强制更新
-    //3.选择创建项目或者组件
+    
+    return this.getProjectInfo();
+
     //3.获取项目的基本信息
     //4. 获取项目的基本信息
   }
 
+ async getProjectInfo(){
+     let projectInfo={};
+     //1.选择创建项目或者组件
+     const { type }=await inquirer.prompt({
+      type:"list",
+      name:"type",
+      message:"请选择初始化类型",
+      default:TYPE_PROJECT,
+      choices:[{
+        name:"项目",
+        value:TYPE_PROJECT
+      },{
+        name:"组件",
+        value:TYPE_COMPONENT
+      }]
+    })
+    log.verbose('type',type)
+    // 2.获取项目基本信息
+    if(type===TYPE_PROJECT){
+      const o=await inquirer.prompt([{
+        type:"input",
+        name:"projectName",
+        message:"请输入项目名称",
+        default:"",
+        validate:function(v){
+          //1. 输入的首字符必须为英文字符
+          //2. 字符必须为英文或数字 不能为字符
+          //3.字符仅允许-_
+          const done = this.async();
+
+          setTimeout(function() {
+            if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+              done('请输入正确的项目名称');
+              return;
+            }
+            // Pass the return value in the done callback
+            done(null, true);
+          }, 0);
+        
+        },
+        filter:(v)=>{
+          return v
+        }
+      },{
+        type:"input",
+        name:"projectVersion",
+        message:"请输入项目版本号",
+        default:"1.0.0",
+        validate:function(v){
+          const done = this.async();
+
+          setTimeout(function() {
+            if (!(!!semver.valid(v))) {
+              done('请输入正确的版本号');
+              return;
+            }
+            // Pass the return value in the done callback
+            done(null, true);
+          }, 0);
+          return
+          // return  /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v);
+        },
+        filter:(v)=>{
+          if(!!semver.valid(v)){
+            return semver.valid(v)
+
+          }else{
+            return v
+          }
+        }
+      }])
+     projectInfo={
+      type,
+      ...o
+     }
+    }else if(type===TYPE_COMPONENT){
+
+    }
+    console.log(projectInfo);
+    return projectInfo
+  }
   // 判断目录是否为空
   isDirEmpty(localPath) {
     let fileList = fs.readdirSync(localPath)
